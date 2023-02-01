7fdc2a942266ffdcba23b866687cd87c75cff8da
carolynz <hi@carolynzhang.com>
2023-01-31 20:29:50 -0800

    More formatting adjustments to poem printout header + footer

--- camtest.py
+++ camtest.py
@@ -1,5 +1,4 @@
 # From picamera2 examples: capture_jpeg.py 
-
 #!/usr/bin/python3
 
 # Capture a JPEG while still running in the preview mode. When you
@@ -10,38 +9,60 @@ import requests
 
 from picamera2 import Picamera2, Preview
 from Adafruit_Thermal import *
+from datetime import datetime
 
+#instantiate printer
 printer = Adafruit_Thermal('/dev/serial0', 19200, timeout=5)
 
+#instantiate camera
 picam2 = Picamera2()
 
-# FOR DEBUGGING: computer preview of image
-# preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
-# picam2.configure(preview_config)
-# picam2.start_preview(Preview.QTGL)
+# NEXT 3 LINES FOR DEBUGGING: computer preview of image
+# Slows down the program considerably
+# Only use for checking camera object
+#preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
+#picam2.configure(preview_config)
+#picam2.start_preview(Preview.QTGL)
 
+# start camera
 picam2.start()
 time.sleep(2)
 
-# Takes photo & saves it
-# TODO: make saved images have a unique filename
+# Take photo & save it
 metadata = picam2.capture_file('images/test.jpg')
+
 # FOR DEBUGGING: print metadata
 #print(metadata)
+
 # FOR DEBUGGING: note that image has been saved
 print('----- SUCCESS: image saved locally')
 
-# receipt printer prints status update
-printer.println('\n\nAI poetry camera')
-#printer.println('\n\nThinking...')
-#printer.println('\n\nWriting...')
-
-# Takes a photo and saves to memory
+# TODO? Takes a photo and saves to memory
 #image = picam2.capture_image()
 #print(image)
 
 # Close camera
 picam2.close()
+
+#######################
+# Receipt printer:
+# Date/time/location frontmatter
+#######################
+
+# Get current date+time -- will use for printing and file naming
+now = datetime.now()
+
+# Format printed datetime like:
+# Jan 1, 2023
+# 8:11 PM
+date_string = now.strftime('%b %-d, %Y')
+time_string = now.strftime('%-I:%M %p')
+printer.println('\n')
+printer.println(date_string)
+printer.println(time_string)
+
+# TODO: get and print location
+
 
 #########################
 # Send saved image to API
@@ -56,7 +77,8 @@ with open('images/test.jpg', 'rb') as f:
   byte_im = f.read()
   
   # prep format for API call
-  files = {'file': ('test.jpg', byte_im, 'image/jpg')}
+  image_filename = 'rpi-' + now.strftime('%Y-%m-%d-at-%I.%M-%p')
+  files = {'file': (image_filename, byte_im, 'image/jpg')}
 
 # Send byte array in API
 response = requests.post(api_url, files=files)
@@ -65,7 +87,19 @@ response_data = response.json()
 # FOR DEBUGGING: print response to console for debugging
 # print(response_data)
 
-# print poem!!!
-printer.println('\n')
+############
+# print poem
+############
 printer.println(response_data['poem'])
+printer.println('\n')
+
+##############
+# print footer
+##############
+printer.justify('C') # center align footer text
+printer.println('------------------')
+printer.println('\n')
+printer.println(' This poem was written by AI.\n')
+printer.println('Learn more at')
+printer.println('poetry.camera')
 printer.println('\n\n\n\n')
