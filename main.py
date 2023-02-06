14c396ca8ddc1f78dbf1b7e4296ebe3030d056cd
carolynz <hi@carolynzhang.com>
2023-02-05 18:21:16 -0800

    shutter button working; somehow, hitting Ctrl+C shuts down my pi; need to fix bug

--- main.py
+++ main.py
@@ -4,11 +4,13 @@
 # Capture a JPEG while still running in the preview mode. When you
 # capture to a file, the return value is the metadata for that image.
 
-import time
-import requests
+import time, requests, signal
 
 from picamera2 import Picamera2, Preview
+from gpiozero import LED, Button
+from sys import exit
 from Adafruit_Thermal import *
+from wraptext import *
 from datetime import datetime
 
 #instantiate printer
@@ -16,99 +18,142 @@ printer = Adafruit_Thermal('/dev/serial0', 19200, timeout=5)
 
 #instantiate camera
 picam2 = Picamera2()
+# start camera
+picam2.start()
+time.sleep(2) # warmup period since first few frames are often poor quality
 
 # NEXT 3 LINES FOR DEBUGGING: computer preview of image
 # Slows down the program considerably
 # Only use for checking camera object
-preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
-picam2.configure(preview_config)
-picam2.start_preview(Preview.QTGL)
+# TODO: WHY DOES THIS PREVIEW NOT WORK OVER SSH?
+#preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
+#picam2.configure(preview_config)
+#picam2.start_preview(Preview.QTGL)
 
-# start camera
-picam2.start()
-time.sleep(2)
+#instantiate buttons
+shutter_button = Button(21)
+power_button = Button(26, hold_time = 2)
 
-# Take photo & save it
-metadata = picam2.capture_file('images/test.jpg')
+#############################
+# CORE PHOTO-TO-POEM FUNCTION
+#############################
+def take_photo_and_print_poem():
+  # Take photo & save it
+  metadata = picam2.capture_file('images/image.jpg')
 
-# FOR DEBUGGING: print metadata
-#print(metadata)
+  # FOR DEBUGGING: print metadata
+  #print(metadata)
 
-# FOR DEBUGGING: note that image has been saved
-print('----- SUCCESS: image saved locally')
+  # TODO? Takes a photo and saves to memory
+  #image = picam2.capture_image()
+  #print(image)
 
-# TODO? Takes a photo and saves to memory
-#image = picam2.capture_image()
-#print(image)
+  # Close camera
+  # picam2.close()
 
-# Close camera
-picam2.close()
+  # FOR DEBUGGING: note that image has been saved
+  print('----- SUCCESS: image saved locally')
 
-#######################
-# Receipt printer:
-# Date/time/location frontmatter
-#######################
+  #######################
+  # Receipt printer:
+  # Date/time/location frontmatter
+  #######################
 
-# Get current date+time -- will use for printing and file naming
-now = datetime.now()
+  # Get current date+time -- will use for printing and file naming
+  now = datetime.now()
 
-# Format printed datetime like:
-# Jan 1, 2023
-# 8:11 PM
-printer.justify('C') # center align header text
-date_string = now.strftime('%b %-d, %Y')
-time_string = now.strftime('%-I:%M %p')
-printer.println('\n')
-printer.println(date_string)
-printer.println(time_string)
-printer.println()
-
-# TODO: get and print location
-
-printer.println("`'. .'`'. .'`'. .'`'. .'`'. .'`")
-printer.println("   `     `     `     `     `   ")
-
+  # Format printed datetime like:
+  # Jan 1, 2023
+  # 8:11 PM
+  printer.justify('C') # center align header text
+  date_string = now.strftime('%b %-d, %Y')
+  time_string = now.strftime('%-I:%M %p')
+  printer.println('\n')
+  printer.println(date_string)
+  printer.println(time_string)
 
 
-#########################
-# Send saved image to API
-#########################
-api_url = 'https://poetry-camera-prototype.carozee.repl.co/pic_to_poem'
+  # TODO: get and print location
 
-# OLD: get PIL Image object from memory
-# files = {'file': image}
+  # optical spacing adjustments
+  printer.setLineHeight(56) # I want something slightly taller than 1 row
+  printer.println()
+  printer.setLineHeight() # Reset to default (32)
 
-# Read file that was saved to disk as a byte array
-with open('images/test.jpg', 'rb') as f:
-  byte_im = f.read()
-  
-  # prep format for API call
-  image_filename = 'rpi-' + now.strftime('%Y-%m-%d-at-%I.%M-%p')
-  files = {'file': (image_filename, byte_im, 'image/jpg')}
+  printer.println("`'. .'`'. .'`'. .'`'. .'`'. .'`")
+  printer.println("   `     `     `     `     `   ")
 
-# Send byte array in API
-response = requests.post(api_url, files=files)
-response_data = response.json()
+  #########################
+  # Send saved image to API
+  #########################
+  api_url = 'https://poetry-camera-prototype.carozee.repl.co/pic_to_poem'
 
-# FOR DEBUGGING: print response to console for debugging
-# print(response_data)
+  # OLD: get PIL Image object from memory
+  # files = {'file': image}
 
-############
-# print poem
-############
-printer.justify('L') # left align poem text
-printer.println(response_data['poem'])
-printer.println()
+  # Read file that was saved to disk as a byte array
+  with open('images/image.jpg', 'rb') as f:
+    byte_im = f.read()
+
+    # prep format for API call
+    image_filename = 'rpi-' + now.strftime('%Y-%m-%d-at-%I.%M-%p')
+    files = {'file': (image_filename, byte_im, 'image/jpg')}
+
+  # Send byte array in API
+  response = requests.post(api_url, files=files)
+  response_data = response.json()
+
+  # FOR DEBUGGING: print response to console for debugging
+  #print(response_data['poem'])
+
+  ############
+  # print poem
+  ############
+
+  # wrap text to 32 characters per line (max width of receipt printer)
+  printable_poem = wrap_text(response_data['poem'], 32)
+
+  printer.justify('L') # left align poem text
+  printer.println(printable_poem)
+
+  ##############
+  # print footer
+  ##############
+  printer.justify('C') # center align footer text
+  printer.println("   .     .     .     .     .   ")
+  printer.println("_.` `._.` `._.` `._.` `._.` `._")
+  printer.println('\n')
+  printer.println(' This poem was written by AI.')
+  printer.println()
+  printer.println('Explore the archives at')
+  printer.println('poetry.camera')
+  printer.println('\n\n\n\n')
+
+  print('-----DONE PRINTING')
+  return
 
 ##############
-# print footer
+# POWER BUTTON
 ##############
-printer.justify('C') # center align footer text
-printer.println("   .     .     .     .     .   ")
-printer.println("_.` `._.` `._.` `._.` `._.` `._")
-printer.println('\n')
-printer.println(' This poem was written by AI.')
-printer.println()
-printer.println('Explore the archives at')
-printer.println('poetry.camera')
-printer.println('\n\n\n\n')
+def shutdown():
+  print('shutdown button held for 2s')
+#  check_call(['sudo','poweroff'])
+
+#################################
+# For RPi debugging:
+# Handle Ctrl+C script termination gracefully
+# (Otherwise, it shuts down the entire Pi -- bad)
+#################################
+def handle_keyboard_interrupt(sig, frame):
+  print('Ctrl+C received, stopping script')
+  exit(0)
+
+signal.signal(signal.SIGINT, handle_keyboard_interrupt)
+
+################################
+# LISTEN FOR BUTTON PRESS EVENTS
+################################
+shutter_button.when_pressed = take_photo_and_print_poem
+power_button.when_held = shutdown
+
+signal.pause()
