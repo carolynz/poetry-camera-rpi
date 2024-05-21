dab7daceb1304a46bb92b47184a4d235a1e46e8b
carolynz <hi@carolynzhang.com>
2024-05-21 13:55:15 -0400

    [untested] wait for working wifi before git pull

--- updater.sh
+++ updater.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
+
+
+# Function to check internet connectivity
+check_internet() {
+    wget -q --spider http://google.com
+    if [ $? -eq 0 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Wait until an internet connection is available
+until check_internet; do
+    echo "Waiting for internet connection..."
+    sleep 5
+done
+
 cd /home/carolynz/CamTest
 git pull origin v4-dev  # Adjust branch name if different
+
+echo "Update completed"
